048fa3f15eba9b2982030d5427c605becf89a819
Mickael Mangaud <mickaelmangaud@gmail.com>
2021-01-22 20:35:25 +0100

    vuex state peristence

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from './modules/auth';
 import posts from './modules/posts';
+import VuexPersistence from 'vuex-persist';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 Vue.use(Vuex);
 Vue.config.devtools = true;
@@ -10,5 +15,6 @@ export default new Vuex.Store({
   modules: {
     auth,
     posts
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
